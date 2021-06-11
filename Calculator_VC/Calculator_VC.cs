39e35a79a0e0ff03e9d49dc6e0a74bf3a8da8476
Robert Cavigelli <robert.cavigelli@quibiq.ch>
2021-06-11 10:35:04 +0200

    Implemented multiplication and division, moved calculation and prompting the result to an own method each

--- Calculator_VC/Calculator_VC.cs
+++ Calculator_VC/Calculator_VC.cs
@@ -15,11 +15,17 @@ namespace Calculator_VC
         // Acceptance Criteria: Two numbers with or without decimal places get added correctly
 
         // Iteration II
-        // -----------
+        // ------------
         // User Story "Substraction": User wants to be able to do substractions as well
         // Acceptance Criteria: A number with or without decimal places gets substracted correctly from another number with or without decimal places
         // User Story "Software Structure": Developer wants to structure the source code so that it will be easier to implement additional functions
         // Acceptance Criteria: The principle of single level of abstraction is applied and if not, the exception is justified and documented
+
+        // Iteration II
+        // ------------
+        // User Story "Multiplication, division": User wants to be able to do multiplications and divisions as well
+        // Acceptance Criteria: Multiplications and divisions get done correctly with any numbers with or without decimal places
+
         static void Main(string[] args)
         {
             
@@ -33,26 +39,14 @@ namespace Calculator_VC
             double d_2nd = Convert.ToDouble(s_2nd);
 
             // Calculation
-            double d_result = 0;
             switch (s_operator)
             {
                 case "+":
-                    d_result = Add(d_1st, d_2nd);
-
-                    // Present result
-                    Console.WriteLine("{0} {1} {2} = {3}", d_1st, s_operator, d_2nd, d_result);
-                    break;
-
                 case "-":
-                    d_result = Substract(d_1st, d_2nd);
-
-                    // Present result
-                    Console.WriteLine("{0} {1} {2} = {3}", d_1st, s_operator, d_2nd, d_result);
-                    break;
-
                 case "/":
                 case "*":
-                    Console.WriteLine("Operation '{0}' is not supported yet - but will be soon ;)", s_operator);
+                    // Present result
+                    PresentResult(d_1st, d_2nd, s_operator, Calculate(d_1st, d_2nd, s_operator));
                     break;
 
                 default:
@@ -71,6 +65,40 @@ namespace Calculator_VC
             return s_input;
         }
 
+        static void PresentResult(double d_1st, double d_2nd, string s_operator, double d_result)
+        {
+            Console.WriteLine("{0} {1} {2} = {3}", d_1st, s_operator, d_2nd, d_result);
+        }
+
+        static double Calculate(double d_1st, double d_2nd, string s_operator)
+        {
+            double d_result = 0;
+
+            switch (s_operator)
+            {
+                case "+":
+                    d_result = d_1st + d_2nd;
+                    break;
+
+                case "-":
+                    d_result = d_1st - d_2nd;
+                    break;
+
+                case "*":
+                    d_result = d_1st * d_2nd;
+                    break;
+
+                case "/":
+                    d_result = d_1st / d_2nd;
+                    break;
+
+                default:
+                    break;
+            }
+
+            return d_result;
+        }
+
         static double Add(double d_1st, double d_2nd)
         {
             double d_sum = d_1st + d_2nd;
@@ -80,9 +108,23 @@ namespace Calculator_VC
 
         static double Substract(double d_minuend, double d_subtrahend)
         {
-            double d_result = d_minuend - d_subtrahend;
+            double d_difference = d_minuend - d_subtrahend;
 
-            return d_result;
+            return d_difference;
+        }
+
+        static double Multiply(double d_multiplier, double d_multiplicand)
+        {
+            double d_product = d_multiplier * d_multiplicand;
+
+            return d_product;
+        }
+
+        static double Divide(double d_dividend, double d_divisor)
+        {
+            double d_fraction = d_dividend / d_divisor;
+
+            return d_fraction;
         }
     }
 }
